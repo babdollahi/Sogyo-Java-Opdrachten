dd811872a4ac429caa9054d349b26f9058c12ba7
Banafsheh Abdollahi <babdollahi@sogyo.nl>
2023-02-03 15:49:56 +0100

    Lists assignment part 1, initialize an array with 10 random numbers between 0 and 100

---
+++ randomNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class randomNumbers{
+public static void main(String[] args) 
+{
+    int[] numbers = new int[10];       
+    //Generates 10 Random Numbers in the range 0 and 100
+    for(int i = 0; i < numbers.length; i++) {
+      numbers[i] = (int)(Math.random()*100 + 1);
+    }//end for loop
+    System.out.println("Numbers Generated: " + Arrays.toString(numbers));
+}
+}
