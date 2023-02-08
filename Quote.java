78f4bbcd47a4ab69e1dd29b9fab8858b16fd4517
Banafsheh Abdollahi <babdollahi@sogyo.nl>
2023-02-08 14:25:19 +0100

    Quote of the day assignment compeleted

--- Quote.java
+++ Quote.java
@@ -1,5 +1,7 @@
 //package nl.sogyo.javaopdrachten.quote;
 import java.time.LocalDate;    
+import java.time.Month;
+import java.time.DayOfWeek;
 
 public class Quote
 {
@@ -23,7 +25,7 @@ public class Quote
         }  
         return capitalizeWord.trim();  
     }  
-
+    
     public static void main(String[] args) 
     { 
         String[][] quotes = 
@@ -38,7 +40,20 @@ public class Quote
 
         //Getting the day of year using the current date
         int dayOfYear = LocalDate.now().getDayOfYear();
-        System.out.println("Day of year "+ dayOfYear);
+
+        //Getting day of the week, day, month
+        DayOfWeek currentDayWeek = LocalDate.now().getDayOfWeek();
+        int currentDay = LocalDate.now().getDayOfMonth();
+        Month currentMonth = LocalDate.now().getMonth();
+        
+        //Printing the "Quote for <dayOfWeek> the <day>th of <Month>:"
+        System.out.println("Quote for " + 
+                            capitalizeWord(currentDayWeek.toString().toLowerCase())+ 
+                            " the " + 
+                            currentDay + 
+                            "th of " + 
+                            capitalizeWord(currentMonth.toString().toLowerCase())+ 
+                            ":");
 
         //Determine quote number to print based on the day of the year
         int quotesNum;
@@ -59,17 +74,17 @@ public class Quote
         //Peint the quote 
         switch (quotesNum)
         {
-            case 1:  System.out.println(capitalizeWord(quotes[0][1]) + ". -- " + capitalizeWord(quotes[0][0]));
+            case 1:  System.out.println('\u0022' + capitalizeWord(quotes[0][1]) + "." + '\u0022' + " -- " + capitalizeWord(quotes[0][0]));
                      break;
-            case 2:  System.out.println(capitalizeWord(quotes[1][1]) + " -- " + capitalizeWord(quotes[1][0]));
+            case 2:  System.out.println('\u0022' + capitalizeWord(quotes[1][1]) + '\u0022' + " -- " + capitalizeWord(quotes[1][0]));
                      break;
-            case 3:  System.out.println(capitalizeWord(quotes[2][1])+ ". -- " + capitalizeWord(quotes[2][0]));
+            case 3:  System.out.println('\u0022' + capitalizeWord(quotes[2][1])+ "." + '\u0022' + " -- " + capitalizeWord(quotes[2][0]));
                      break;
-            case 4:  System.out.println(capitalizeWord(quotes[3][1])+ ". -- " + capitalizeWord(quotes[3][0]));
+            case 4:  System.out.println('\u0022' + capitalizeWord(quotes[3][1])+ "." + '\u0022' + " -- " + capitalizeWord(quotes[3][0]));
                      break;
-            case 5:  System.out.println(capitalizeWord(quotes[4][1])+ ". -- " + capitalizeSentence(quotes[4][0]));
+            case 5:  System.out.println('\u0022' + capitalizeWord(quotes[4][1])+ "." + '\u0022' + " -- " + capitalizeSentence(quotes[4][0]));
                      break;
-            case 0:  System.out.println(capitalizeWord(quotes[5][1])+ ". -- " + capitalizeSentence(quotes[5][0]));
+            case 0:  System.out.println('\u0022' + capitalizeWord(quotes[5][1]) + "." + '\u0022' + " -- " + capitalizeSentence(quotes[5][0]));
                     break;
         }
     }  
