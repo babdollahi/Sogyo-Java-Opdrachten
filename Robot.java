84b45df70c1378f4022cc2af909e51d27ab1f79c
Banafsheh Abdollahi <babdollahi@sogyo.nl>
2023-02-10 18:10:27 +0100

    Modified forward and backward method

--- Robot.java
+++ Robot.java
@@ -1,3 +1,4 @@
+//https://www.tutorialspoint.com/java/java_object_classes.htm
 import java.io.*;
 public class Robot {
 
@@ -5,6 +6,25 @@ public class Robot {
    String head;
    int x;
    int y;
+   //private static int mv;
+   //int speed;
+
+   /*public void setSpeed(int mv) 
+   {  
+      speed = mv;
+   }
+   public int getSpeed()
+   {
+      return speed;
+   }*/
+   public static int setSpeed(int speed) {
+      //for (int i = 0; i < 4; i++) 
+      //{
+         speed = 10;
+         return speed;
+     // }
+  }
+  
 
    public Robot(String name, int x, int y, String head) 
    {
@@ -13,7 +33,7 @@ public class Robot {
       this.y = y;
       this.head = head;
    }
-   /* Print the Employee details */
+   // Print the Robot details
    public void printRobot() 
    {
       System.out.println("Initial position and facing of " + name +
@@ -22,11 +42,13 @@ public class Robot {
       " , " + head);
    }
 
+   // Print the state of the Robot
    public void printState() 
    {
       System.out.println( "Now " + name + " is facing "+ head + " at " + "(" + x + "," + y + ")");
-   }// Now facing "North"
+   }
 
+   // Creating the left function
    public void turnLeft()
    {
       switch (head)
@@ -43,6 +65,7 @@ public class Robot {
       System.out.println( name + " turned Left.");
    }
 
+   // Creating the right function
    public void turnRight()
    {
       switch (head)
@@ -59,37 +82,53 @@ public class Robot {
       System.out.println(name + " turned Right.");
    }
 
+   //Creating the forward function
    public void forward()
    {
-      y += 1;
+      switch (head)
+      {
+        case "East":  x = x + setSpeed(2);
+        break;
+        case "South":  y = y - setSpeed(2);
+        break;
+        case "West": x = x - setSpeed(2);
+        break;
+        case "North": y = y + setSpeed(2);
+        break;
+      } 
       System.out.println( name + " moved Forward.");
    }
-
+   //Creating the backward function
    public void backward()
    {
-      x += 1;
+      switch (head)
+      {
+        case "East":  x = x - 1;
+        break;
+        case "South":  y = y + 1;
+        break;
+        case "West": x = x + 1;
+        break;
+        case "North": y = y - 1;
+        break;
+      }       
       System.out.println( name + " moved Backward.");
    }
 
 
-
    public static void main(String args[]) 
    {
-      /* Create two objects using constructor */
-      //Robot defaultRobot = new Robot("defaultRobot", 0, 0, "North");
-      //defaultRobot.printRobot();
+      //Create two Robots using constructor
       Robot myFirstRobot = new Robot("myFirstRobot", 0, 1, "East");
       myFirstRobot.printRobot();
       Robot mySecondRobot = new Robot("mySecondRobot", 1, 0, "West");
       mySecondRobot.printRobot();
+
       myFirstRobot.turnLeft();
       myFirstRobot.forward();
       myFirstRobot.printState();
-
       mySecondRobot.backward();
       mySecondRobot.printState();
-
-
 
    }
 }
