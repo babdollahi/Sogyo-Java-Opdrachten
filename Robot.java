987dbe1d2b1d2d217f4060f75d9e7d2da802c567
Banafsheh Abdollahi <babdollahi@sogyo.nl>
2023-02-13 16:27:58 +0100

    Roborally compeleted

--- Robot.java
+++ Robot.java
@@ -1,31 +1,18 @@
-//https://www.tutorialspoint.com/java/java_object_classes.htm
+//Banafsheh Abdollahi wrote this programm for Roborally assignment.
 import java.io.*;
-public class Robot {
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class Robot implements Runnable 
+{
+   //Defining variables
    String name;
    String head;
    int x;
    int y;
-   //private static int mv;
-   //int speed;
 
-   /*public void setSpeed(int mv) 
-   {  
-      speed = mv;
-   }
-   public int getSpeed()
-   {
-      return speed;
-   }*/
-   public static int setSpeed(int speed) {
-      //for (int i = 0; i < 4; i++) 
-      //{
-         speed = 10;
-         return speed;
-     // }
-  }
-  
-
+   //Defining robots with the name, positon (x ,y) and facing (head)
    public Robot(String name, int x, int y, String head) 
    {
       this.name = name;
@@ -33,7 +20,8 @@ public class Robot {
       this.y = y;
       this.head = head;
    }
-   // Print the Robot details
+
+   // Printing the Robot details
    public void printRobot() 
    {
       System.out.println("Initial position and facing of " + name +
@@ -42,15 +30,33 @@ public class Robot {
       " , " + head);
    }
 
-   // Print the state of the Robot
+   // Printing the state of the Robot
    public void printState() 
    {
       System.out.println( "Now " + name + " is facing "+ head + " at " + "(" + x + "," + y + ")");
    }
 
-   // Creating the left function
-   public void turnLeft()
+   // Defineing a list to store all the commands and execute later
+   List<Runnable> commands = new ArrayList<>();
+
+   //Exacute command 
+   public void run()
    {
+      execute();
+   }
+   public void execute()
+   {
+      for (Runnable command: commands)
+      {
+         command.run();
+      }
+   }
+
+
+   // Creating the turn left function
+   public void turnLeft()
+   {  //Adding the command to the list of commands
+      commands.add( () -> {
       switch (head)
       {
         case "East":  head = "North";
@@ -62,12 +68,13 @@ public class Robot {
         case "South": head = "East";
         break;
       } 
-      System.out.println( name + " turned Left.");
+      System.out.println( name + " turned Left.");});
    }
 
-   // Creating the right function
+   // Creating the turn right function
    public void turnRight()
-   {
+   {  //Adding the command to the list of commands
+       commands.add( () -> {
       switch (head)
       {
         case "East":  head = "Suoth";
@@ -79,28 +86,35 @@ public class Robot {
         case "North": head = "East";
         break;
       } 
-      System.out.println(name + " turned Right.");
+      System.out.println(name + " turned Right.");});
    }
 
-   //Creating the forward function
-   public void forward()
-   {
+   //Creating the forward function and set parameters
+   //The defualt parameter is 1 but can be changed
+   public void forward(){this.forward(1);}
+
+   public void forward(int speed)
+   {  //Adding the command to the list of commands
+      commands.add( () -> {
       switch (head)
       {
-        case "East":  x = x + setSpeed(2);
+        case "East":  x = x + speed;
         break;
-        case "South":  y = y - setSpeed(2);
+        case "South":  y = y - speed;
         break;
-        case "West": x = x - setSpeed(2);
+        case "West": x = x - speed;
         break;
-        case "North": y = y + setSpeed(2);
+        case "North": y = y + speed;
         break;
       } 
-      System.out.println( name + " moved Forward.");
+      System.out.println( name + " moved Forward.");});
+
    }
+
    //Creating the backward function
    public void backward()
-   {
+   {  //Adding the command to the list of commands
+      commands.add( () -> {
       switch (head)
       {
         case "East":  x = x - 1;
@@ -112,11 +126,11 @@ public class Robot {
         case "North": y = y - 1;
         break;
       }       
-      System.out.println( name + " moved Backward.");
+      System.out.println( name + " moved Backward.");});
    }
 
-
-   public static void main(String args[]) 
+//Main 
+   public static void main(String args[])
    {
       //Create two Robots using constructor
       Robot myFirstRobot = new Robot("myFirstRobot", 0, 1, "East");
@@ -124,11 +138,25 @@ public class Robot {
       Robot mySecondRobot = new Robot("mySecondRobot", 1, 0, "West");
       mySecondRobot.printRobot();
 
+      //giving the commands from the list of commands 
+      //Before execute the commands are not applied
       myFirstRobot.turnLeft();
       myFirstRobot.forward();
       myFirstRobot.printState();
+
+      mySecondRobot.backward();
+      mySecondRobot.printState();
+
+      //After execute the commands are applied
+      myFirstRobot.execute();
+      myFirstRobot.turnLeft();
+      myFirstRobot.forward();
+      myFirstRobot.printState();
+
+      mySecondRobot.execute();
       mySecondRobot.backward();
       mySecondRobot.printState();
 
    }
 }
+//End
