96a06bf6211059c31edfdd52e88e259656d220bb
Banafsheh Abdollahi <babdollahi@sogyo.nl>
2023-02-15 17:08:38 +0100

    Assignment decision tree compeleted

--- DecisionTree.java
+++ DecisionTree.java
@@ -23,24 +23,20 @@ public class  DecisionTree
 	 */
 	public static void main(String[] args) throws FileNotFoundException 
 	{
+        //Definening variables
+        int idInt;
+
+        //Difening lists
 		ArrayList<String> lines = new ArrayList<String>();
-        // ArrayList<String> words = new ArrayList<String>();
-		// ArrayList<String> ids = new ArrayList<String>();
-		// ArrayList<String> nodes = new ArrayList<String>();
-		int idInt;
-		// int lineNumber;
-		// Integer leftEdgeInt;
+        List<Integer> listNodeId = new ArrayList<Integer>();
 
-		List<Integer> listNodeId = new ArrayList<Integer>();
-		// Map<String, String> node = new HashMap<String, String>();
+        //Difening maps
 		Map<Integer, String> node = new HashMap<Integer, String>();
-
 		Map<String, String> left = new HashMap<String, String>();
 		Map<String, String> right = new HashMap<String, String>();
 		Map<String, String> leaf = new HashMap<String, String>();
 
-
-        // Scanner s = new Scanner(new File("/home/banafsheh/Learning/Java/decision-tree-data.txt")).useDelimiter(Pattern.compile("(\\n)|,"));
+        //Reading the file
 		Scanner s = new Scanner(new File("/home/banafsheh/Learning/Java/decision-tree-data.txt")).useDelimiter(Pattern.compile("(\\n)"));
 
 		// Read the file line by line 
@@ -53,27 +49,23 @@ public class  DecisionTree
 		for (int i = 0; i < lines.size();i++) 
 	      { 	
 			String str = lines.get(i);
-			// lineNumber = i;
 
-			//If the line ends with ?, this line is a node line, 
-			//extract node id and question
+			//If the line ends with ?, this line is a node line, extract node id and question
 			if (String.valueOf(str.charAt(str.length() - 1)).equals("?"))
 			{
 				idInt = Character.getNumericValue(str.charAt(1));
                 listNodeId.add(idInt);
-				// String nodeId = str.split(",")[0];
 				String questionNode = str.split(",")[1];
 				node.put(idInt, questionNode.trim());
 			}
+            //If the line ends with Nee, this line is a left edge line, extract ids
 			else if (str.endsWith("Nee"))
 			{
 				String whatConnectedLeft = str.split(",")[0];
 				String leftEdge = str.split(",")[1];
 				left.put(whatConnectedLeft.trim(), leftEdge.trim());
-				// System.out.println(leftEdge);
-
-
 			}
+            //If the line ends with Ja, this line is a left edge line, extract ids
 			else if (str.endsWith("Ja"))
 			{
 				String whatConnectedRight = str.split(",")[0];
@@ -81,8 +73,8 @@ public class  DecisionTree
 				right.put(whatConnectedRight.trim(), rightEdge.trim());
 			}
 			else 
+            //If not a bove, this line is a leaf, extract leaf id and its value
 			{
-				// String leafValue = str.split(",")[1];
 				String leafId = str.split(",")[0];
 				String leafValue = str.split(",")[1];
 				leaf.put(leafId.trim(), leafValue.trim());
@@ -91,30 +83,21 @@ public class  DecisionTree
 
 		}   
 
-    //Sort node ids to start from the root
-    Collections.sort(listNodeId);
-    for (int i = 0; i < listNodeId.size(); i++) 
-    {
-		System.out.println("*************** Start Node **************");
-		String nodeId = "N" + String.valueOf(listNodeId.get(i));
-		System.out.println("Node ID : " + nodeId);
-		System.out.println("Node question : " + node.get(listNodeId.get(i)));
-		System.out.println("Node left child: " + left.get(nodeId));
-		System.out.println("Node right child: " + right.get(nodeId));
-		System.out.println("Leaf at right: " + leaf.get(left.get(nodeId)));
-		System.out.println("Leaf at left: " + leaf.get(right.get(nodeId)));
-		System.out.println("*************** End Node ****************");
-
-	// }
-}
-
+        //Sort node ids to start from the root
+        Collections.sort(listNodeId);
+        //Printing the tree
+        for (int i = 0; i < listNodeId.size(); i++) 
+        {
+            System.out.println("*************** Start Node **************");
+            String nodeId = "N" + String.valueOf(listNodeId.get(i));
+            System.out.println("Node ID : " + nodeId);
+            System.out.println("Node question : " + node.get(listNodeId.get(i)));
+            System.out.println("Node left child ID : " + left.get(nodeId));
+            System.out.println("Leaf at left: " + leaf.get(right.get(nodeId)));
+            System.out.println("Node right child ID : " + right.get(nodeId));
+            System.out.println("Leaf at right: " + leaf.get(left.get(nodeId)));
+            System.out.println("*************** End Node ****************");
+        }
     }
 }
 
-
-
-			// System.out.println(String.valueOf(str.charAt(str.length() - 1)));
-
-			// String first = String.valueOf((str.charAt(0)));
-			// System.out.println(first);
-			// String last = String.valueOf(str.charAt(str.length() - 1));
